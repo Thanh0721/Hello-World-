6a40f16eb6e04ca3ca507d25188dd20fcbcb5ee6
Thanh0721 <thanhn2789@nycstudents.net>
2024-09-08 15:14:50 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Hello World!");
+        System.out.println("My name is Thanh and this is a new line.");
+    }
+}
